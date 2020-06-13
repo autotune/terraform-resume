9307d0de044a30ae03d9136d5bf17e5a39d5fd73
Brian Adams <service@contrasting.org>
2020-06-13 12:09:33 -0400

    Format main

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
 
+  count = 2
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
