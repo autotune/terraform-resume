113ef97afc6c2e318e0314d7eacba37987554e04
Brian Adams <service@contrasting.org>
2020-06-13 21:32:21 -0400

    Move jenkins

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@
 variable "project" {
   description = "The project ID where all resources will be launched."
   type        = string
+  default = "badamscka"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
