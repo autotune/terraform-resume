6288f0f7c8e66422c4b6e761944796ae96f31826
Brian Adams <service@contrasting.org>
2020-06-14 15:29:40 -0400

    Add kubernetes provider

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@
 variable "project" {
   description = "The project ID where all resources will be launched."
   type        = string
-  default     = "badamscka"
+  default     = ""
 }
 
 variable "cf_email" {
